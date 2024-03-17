6b42f162f7ac087a7182a5ee6ee300bd70b2c699
Lauris <lauris.locmelis@awave.com>
2024-03-17 19:02:10 +0200

    View

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,24 +9,26 @@ use Carbon\Carbon;
 class MainController extends Controller
 {
     public function index() {
-        $sunsets = \App\Models\Sunset::orderBy('id', 'desc')->first();
 
-        $sunsetAt = null;
-        if ($sunsets) {
-            $sunsetAt = Carbon::parse($sunsets->sunset_at)->timezone('Europe/Riga')->format('H:i:s');
-        }
-
-        $sunsetAfter = Sunset::getMinutesUntilSunset();
-
-        $govee = new GoveeController();
-        $state = $govee->getState();
-        $current_time = Carbon::now()->timezone('Europe/Riga')->format('H:i:s');
-
-        return view('welcome', [
-            'sunset' => $sunsetAfter,
-            'sunset_at' => $sunsetAt,
-            'currentlyLights' => $state,
-            'current_time' => $current_time
-        ]);
+        return ['... ' => 'ok ...'];
+//        $sunsets = \App\Models\Sunset::orderBy('id', 'desc')->first();
+//
+//        $sunsetAt = null;
+//        if ($sunsets) {
+//            $sunsetAt = Carbon::parse($sunsets->sunset_at)->timezone('Europe/Riga')->format('H:i:s');
+//        }
+//
+//        $sunsetAfter = Sunset::getMinutesUntilSunset();
+//
+//        $govee = new GoveeController();
+//        $state = $govee->getState();
+//        $current_time = Carbon::now()->timezone('Europe/Riga')->format('H:i:s');
+//
+//        return view('welcome', [
+//            'sunset' => $sunsetAfter,
+//            'sunset_at' => $sunsetAt,
+//            'currentlyLights' => $state,
+//            'current_time' => $current_time
+//        ]);
     }
 }
